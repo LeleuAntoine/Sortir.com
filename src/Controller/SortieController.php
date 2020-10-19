02ec13eb61019f13895a6638ef4b152b52bbebe0
Adalrik <ant.82@hotmail.fr>
2020-10-19 17:37:17 +0200

    Controller + template sortie
    Modification template index sortie + Création template afficher et creer sortie.
    Création FormType sortie.
    Update SortieController

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -13,12 +19,51 @@ use Symfony\Component\Routing\Annotation\Route;
 class SortieController extends AbstractController
 {
     /**
-     * @Route("/sortie", name="sortie")
+     * @Route("/", name="sortie")
      */
-    public function index()
+    public function index(SortieRepository $sortieRepository): Response
     {
-        return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
+        $sorties = $sortieRepository->findBy([], ['dateHeureDebut' => 'DESC']);
+        return $this->render('sortie/index.html.twig', ['sorties' => $sorties]);
+    }
+
+    /**
+     * @Route ("/creer", name="app_sortie_creer", methods={"GET", "POST"})
+     */
+    public function creer(Request $request, EntityManagerInterface $em): Response
+    {
+        $sortie = new Sortie;
+
+        $form = $this->createForm(SortieType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $sortie->setNom($data['nom']);
+            $sortie->setOrganisateur(1);
+            $sortie->setDateHeureDebut($data['datedebut']);
+            $sortie->setDateLimiteInscription($data['datecloture']);
+            $sortie->setNbInscriptionMax($data['nbinscriptionsmax']);
+            $sortie->setDuree($data['duree']);
+            $sortie->setInfosSortie($data['descriptioninfos']);
+            $sortie->setLieux($data['nom_lieu']);
+            $sortie->setEtat(1);
+            $em->persist($sortie);
+            $em->flush();
+
+//            return $this->redirectToRoute('');
+        }
+        return $this->render('sortie/creer.html.twig', [
+            'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/{id<[0-9]+>}", name="app_sortie_afficher", methods={"GET"})
+     */
+    public function afficher(Sortie $sortie): Response
+    {
+        return $this->render('sortie/afficher.html.twig', compact('sortie'));
+    }
+
 }
