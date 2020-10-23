4721c8f50eadfddcd515b4c6f928439f3742455d
Adalrik <ant.82@hotmail.fr>
2020-10-23 17:06:19 +0200

    Ajout class bootstrap

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -126,7 +126,6 @@ class SortieController extends AbstractController
             $sortie->setSiteOrganisateur($participant->getCampus());
             $this->em->persist($sortie);
             $this->em->flush();
-
             return $this->redirectToRoute('app_sortie_index');
         }
         return $this->render('sortie/modifier.html.twig', [
