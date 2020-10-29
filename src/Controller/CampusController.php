f03307021c8c0897c495877a545ae5bad3e81094
Adalrik <ant.82@hotmail.fr>
2020-10-29 20:35:27 +0100

    Campus et ville
    Ajout des fonctionnalité ajout et suppréssion Campus et Ville

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -3,41 +3,62 @@
 namespace App\Controller;
 
 use App\Entity\Campus;
-use App\Form\SortieType;
+use App\Form\CampusType;
 use App\Repository\CampusRepository;
+use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use MercurySeries\FlashyBundle\FlashyNotifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use function Sodium\add;
 
 class CampusController extends AbstractController
 {
+    private $flashy;
+    private $em;
+
+    /**
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(FlashyNotifier $flashy, EntityManagerInterface $em)
+    {
+        $this->flashy = $flashy;
+        $this->em = $em;
+    }
+
     /**
      * @Route("/campus", name="campus")
      */
-    public function index(CampusRepository $campusRepository, FlashyNotifier $flashy,
-                          PaginatorInterface $paginator, Request $request,
-                          EntityManagerInterface $em)
+    public function index(CampusRepository $campusRepository, PaginatorInterface $paginator,
+                          Request $request)
     {
+        $campus = new Campus();
         $utilisateur = $this->getUser();
 
         if (in_array("ROLE_ADMIN", $utilisateur->getRoles())) {
-
             $filtreMot = $request->query->get('nom_campus_contient');
-            $ajoutNomCampus = $request->query->get('nom_campus');
 
+            $form = $this->createForm(CampusType::class, $campus);
+            $form->handleRequest($request);
 
-            dump($ajoutNomCampus);
-            if ($request->query->get('ajouter')) {
-                $nouveauCampus = new Campus();
-                $nouveauCampus->setNom($ajoutNomCampus);
-                $em->persist($nouveauCampus);
-                $em->flush();
+            if ($form->isSubmitted() && $form->isValid()) {
+                $exist = true;
+                $campusTab = $campusRepository->findAll();
+
+                foreach ($campusTab as $c) {
+                    if ($campus->getNom() === $c->getNom())
+                    {
+                        $exist = false;
+                        $this->flashy->error('Ce campus éxiste déjà');
+                    }
+                }
+                if ($exist) {
+                    $this->em->persist($campus);
+                    $this->em->flush();
+                    $this->flashy->success('Campus crée avec succé !');
+                }
             }
-
             $campusTab = $paginator->paginate(
                 $campusRepository->trouverCampusAvecFiltre($filtreMot),
                 $request->query->getInt('page', 1),
@@ -46,9 +67,33 @@ class CampusController extends AbstractController
 
             return $this->render('campus/index.html.twig', [
                 'campusTab' => $campusTab,
+                'form' => $form->createView()
             ]);
         } else {
-            $flashy->error('Vous ne disposez pas des droits nécessaire !');
+            $this->flashy->error('Vous ne disposez pas des droits nécessaire !');
+            return $this->redirectToRoute('app_sortie_index');
+        }
+    }
+
+    /**
+     * @Route("/{id}/supprimer", name="app_campus_supprime", requirements={"id": "\d+"})
+     */
+    public function supprimerCampus(Campus $campus, ParticipantRepository $participantRepository)
+    {
+        $utilisateur = $this->getUser();
+
+        if (in_array("ROLE_ADMIN", $utilisateur->getRoles())) {
+            $participant = $participantRepository->findOneBy(array('campus' => $campus));
+            if (empty($participant)) {
+                $this->em->remove($campus);
+                $this->em->flush();
+                $this->flashy->success('Campus supprimée avec succès !');
+            } else {
+                $this->flashy->error('Ce campus ne peut pas être supprimé');
+            }
+            return $this->redirectToRoute('campus');
+        } else {
+            $this->flashy->error('Vous ne disposez pas des droits nécessaires !');
             return $this->redirectToRoute('app_sortie_index');
         }
     }
--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Ville;
+use App\Form\VilleType;
+use App\Repository\LieuRepository;
 use App\Repository\VilleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -13,28 +15,49 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class VilleController extends AbstractController
 {
+    private $flashy;
+    private $em;
+
+    /**
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(FlashyNotifier $flashy, EntityManagerInterface $em)
+    {
+        $this->flashy = $flashy;
+        $this->em = $em;
+    }
 
     /**
      * @Route("/ville", name="ville")
      */
-    public function index(VilleRepository $villeRepository, FlashyNotifier $flashy,
-                          PaginatorInterface $paginator, Request $request,
-                          EntityManagerInterface $em)
+    public function index(VilleRepository $villeRepository, PaginatorInterface $paginator,
+                          Request $request)
     {
+        $ville = new Ville();
         $utilisateur = $this->getUser();
 
-        if(in_array("ROLE_ADMIN", $utilisateur->getRoles())) {
-            $villes = $villeRepository->findAll();
+        if (in_array("ROLE_ADMIN", $utilisateur->getRoles())) {
             $filtreMot = $request->query->get('nom_ville_contient');
-            $ajoutNomVille = $request->query->get('nom_ville');
-            $ajoutCodePostalVille = $request->query->get('code_postal_ville');
 
-            if ($ajoutNomVille and $ajoutCodePostalVille) {
-                $nouvelleVille = new Ville();
-                $nouvelleVille->setNom($ajoutNomVille);
-                $nouvelleVille->setCodePostal($ajoutCodePostalVille);
-                $em->persist($nouvelleVille);
-                $em->flush();
+            $form = $this->createForm(VilleType::class, $ville);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $exist = true;
+                $villes = $villeRepository->findAll();
+
+                foreach ($villes as $v) {
+                    if ($ville->getNom() === $v->getNom() and
+                        $ville->getCodePostal() === $v->getCodePostal()) {
+                        $exist = false;
+                        $this->flashy->error('Cette ville éxiste déjà');
+                    }
+                }
+                if ($exist) {
+                    $this->em->persist($ville);
+                    $this->em->flush();
+                    $this->flashy->success('Ville crée avec succé !');
+                }
             }
 
             $villes = $paginator->paginate(
@@ -45,12 +68,36 @@ class VilleController extends AbstractController
 
             return $this->render('ville/index.html.twig', [
                 'villes' => $villes,
+                'form' => $form->createView()
             ]);
 
         } else {
-            $flashy->error('Vous ne disposez pas des droits nécessaires !' );
+            $this->flashy->error('Vous ne disposez pas des droits nécessaires !');
             return $this->redirectToRoute('app_sortie_index');
         }
 
+    }
+
+    /**
+     * @Route("/{id}/supprimer", name="app_ville_supprime", requirements={"id": "\d+"})
+     */
+    public function supprimerVille(Ville $ville, LieuRepository $lieuRepository)
+    {
+        $utilisateur = $this->getUser();
+
+        if (in_array("ROLE_ADMIN", $utilisateur->getRoles())) {
+            $lieu = $lieuRepository->findOneBy(array('ville' => $ville));
+            if (empty($lieu)) {
+                $this->em->remove($ville);
+                $this->em->flush();
+                $this->flashy->success('Ville supprimée avec succès !');
+            } else {
+                $this->flashy->error('Cette ville ne peut pas être supprimé');
+            }
+            return $this->redirectToRoute('ville');
+        } else {
+            $this->flashy->error('Vous ne disposez pas des droits nécessaires !');
+            return $this->redirectToRoute('app_sortie_index');
+        }
     }
 }
